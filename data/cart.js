d5b6da4a028ec91f32b9d6946b2448b84b46e487
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2024-01-22 21:14:10 +0700

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,8 @@
-export const cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -35,12 +30,16 @@ export function addToCart(productId) {
   } else {
     matchingItem.quantity += quantity;
   }
+
+  saveToStorage();
 }
 
-export function removeFromCart(productId){
+export function removeFromCart(productId) {
   cart.forEach((cartItem, index) => {
-    if(productId === cartItem.productId){
-      cart.splice(index, 1)
+    if (productId === cartItem.productId) {
+      cart.splice(index, 1);
     }
-  })
+  });
+
+  saveToStorage();
 }
